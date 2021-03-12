e9c57041662d8c930cc3386f17b96ffffa8fa995
Dominika Barrett <barrettdomi@gmail.com>
2021-03-12 17:35:22 +0000

    added file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DiceGameClass myGame = new DiceGameClass();
+            myGame.Start();
+        }
+    }
+}
